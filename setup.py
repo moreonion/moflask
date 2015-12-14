252bb58425ecaf01e691a1de0974cbe0c1846f5b
Roman Zimmermann <roman@more-onion.com>
2015-12-14 15:01:56 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='moflask',
+    version='0.1',
+    description='Flask utilities by more-onion.',
+    author='Roman Zimmermann',
+    author_email='torotil@gmail.com',
+    packages=['moflask'],
+)
