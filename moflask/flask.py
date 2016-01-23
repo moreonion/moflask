c95994387752c6a2873133732b57961acd5ec202
Roman Zimmermann <roman@more-onion.com>
2016-01-23 12:13:01 +0100

    flask.BaseApp: Make it possible to override FLASK_ENV.

--- moflask/flask.py
+++ moflask/flask.py
@@ -6,19 +6,20 @@ from werkzeug.utils import import_string
 
 class BaseApp(Flask):
     @staticmethod
-    def config_obj_from_env():
-        env = os.getenv('FLASK_ENV', 'Development').capitalize()
+    def config_obj_from_env(env=None):
+        env = env or os.getenv('FLASK_ENV', 'Development').capitalize()
         path = os.getenv('FLASK_CONFIG_OBJECT', 'settings.' + env + 'Config')
         return import_string(path)()
 
     def __init__(self, *args, **kwargs):
         config = kwargs.pop('config', None)
+        env = kwargs.pop('env', None)
         super().__init__(*args, **kwargs)
         # load app config
         # 1. from an import path specfied in the FLASK_CONFIG_OBJECT
         #    environmentfrom variable. Default: settings.DevelopmentConfig
         # 2. A config object passed as keyword argument: config=Object
-        self.config.from_object(self.config_obj_from_env())
+        self.config.from_object(self.config_obj_from_env(env))
         self.config.from_object(config)
 
         for handler in self.config.get('LOG_HANDLERS', []):
