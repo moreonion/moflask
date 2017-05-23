2942b14e3d8c1e8b134f80d6b4d58f1d12fa2764
Roman Zimmermann <roman@more-onion.com>
2017-05-23 18:03:24 +0200

    Prepare for pypi release.

--- setup.py
+++ setup.py
@@ -5,8 +5,16 @@ from distutils.core import setup
 setup(
     name='moflask',
     version='0.1',
-    description='Flask utilities by more-onion.',
+    description='Re-usable flask utilities.',
     author='Roman Zimmermann',
     author_email='torotil@gmail.com',
     packages=['moflask'],
+    install_requires=['Flask'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+    url='https://github.com/moreonion/moflask',
+    download_url='https://github.com/moreonion/moflask/archive/v0.1.tar.gz',
 )
