e7c1b58852f48d6441eb0d02b5a903a0860c4a91
Katharina Zwinger <katharina@more-onion.com>
2021-08-06 11:52:18 +0200

    Update coding style in flask.py

--- moflask/flask.py
+++ moflask/flask.py
@@ -1,3 +1,5 @@
+"""Basic Flask App with a few enhancements."""
+
 import os
 
 from flask import Flask
@@ -5,15 +7,19 @@ from werkzeug.utils import import_string
 
 
 class BaseApp(Flask):
+    """Flask App."""
+
     @staticmethod
     def config_obj_from_env(env=None):
-        env = env or os.getenv('FLASK_ENV', 'Development').capitalize()
-        path = os.getenv('FLASK_CONFIG_OBJECT', 'settings.' + env + 'Config')
+        """Find a config object that matches the current environment."""
+        env = env or os.getenv("FLASK_ENV", "Development").capitalize()
+        path = os.getenv("FLASK_CONFIG_OBJECT", "settings." + env + "Config")
         return import_string(path)()
 
     def __init__(self, *args, **kwargs):
-        config = kwargs.pop('config', None)
-        env = kwargs.pop('env', None)
+        """Create a new app object."""
+        config = kwargs.pop("config", None)
+        env = kwargs.pop("env", None)
         super().__init__(*args, **kwargs)
         # load app config
         # 1. from an import path specfied in the FLASK_CONFIG_OBJECT
@@ -22,19 +28,23 @@ class BaseApp(Flask):
         self.config.from_object(self.config_obj_from_env(env))
         self.config.from_object(config)
 
-        for handler in self.config.get('LOG_HANDLERS', []):
+        for handler in self.config.get("LOG_HANDLERS", []):
             for logger in [self.logger]:
                 logger.addHandler(handler)
 
         if not self.sanity_check():
-            raise RuntimeError('Sanity checks failed. Aborting!')
+            raise RuntimeError("Sanity checks failed. Aborting!")
 
         self.init_sentry()
 
     def init_sentry(self):
-        if self.config.get('SENTRY_DSN', False):
+        """Initialize Sentry."""
+        if self.config.get("SENTRY_DSN", False):
             from raven.contrib.flask import Sentry
+
             self.sentry = Sentry(self)
 
-    def sanity_check(self):
+    @staticmethod
+    def sanity_check():
+        """Check sanity."""
         return True
