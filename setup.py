e20bbc93ebdc75a722b55c51190311a339b5fd99
Roman Zimmermann <roman@more-onion.com>
2022-04-21 11:48:17 +0200

    Prepare for the 1.3.0 release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name="moflask",
-    version="1.2.2",
+    version="1.3.0",
     description="Re-usable flask utilities.",
     author="Roman Zimmermann",
     author_email="torotil@gmail.com",
@@ -31,5 +31,5 @@ setuptools.setup(
         ],
     },
     url="https://github.com/moreonion/moflask",
-    download_url="https://github.com/moreonion/moflask/archive/refs/tags/v1.2.2.tar.gz",
+    download_url="https://github.com/moreonion/moflask/archive/refs/tags/v1.3.0.tar.gz",
 )
